4f12c7ac42b22cf23aed07fbb6385270002753ed
Ajith <ajith4ab@gmail.com>
2020-07-23 22:23:59 +0530

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-native-amazon-chime
+
+## Currently implementation of SDK is in Progress for react native
---
+++ index.js
@@ -0,0 +1,5 @@
+import { NativeModules } from 'react-native';
+
+const { AmazonChime } = NativeModules;
+
+export default AmazonChime;
